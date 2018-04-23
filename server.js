1d63a4caabb2768d175b300d585de1d918c52a38
rrahulrroy <35720725+rrahulrroy@users.noreply.github.com>
2018-04-23 19:33:31 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one.html',function(req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+});
+
 var counter = 0;
 app.get('/counter',function (req,res){
     counter = counter + 1;
